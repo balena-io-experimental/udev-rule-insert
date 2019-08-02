489993f13cbc39666aef142ab0f14909f278d0ae
Gergely Imreh <gergely@balena.io>
2019-08-02 11:20:21 +0100

    Update the CLI use as some flags are no longer needed
    Change-type: patch
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # * in parallel runs the update logic on them, with parallelism defined by the '-P' setting
 #   Connect to the device with balena ssh in non-interctive mode (so that connection fails if the UUID is not accessible),
 #   pipe in the task script, and save the log with the UUID prepended
-cat batch | stdbuf -oL xargs -I{} -P 30 /bin/sh -c "grep -a -q '{} : DONE' udev-rules.log || (cat add-udev-rule.sh | balena ssh {} -s --noninteractive | sed 's/^/{} : /' | tee --append udev-rules.log)"
+cat batch | stdbuf -oL xargs -I{} -P 30 /bin/sh -c "grep -a -q '{} : DONE' udev-rules.log || (cat add-udev-rule.sh | balena ssh {} | sed 's/^/{} : /' | tee --append udev-rules.log)"
